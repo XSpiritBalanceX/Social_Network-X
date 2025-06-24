4b915a168238d78fdb5973560ff71078b5435abe
Helen <lenagreidon@mail.ru>
2025-06-24 15:24:21 +0300

    updated styles and added classes

--- src/app/(home)/Tweet.tsx
+++ src/app/(home)/Tweet.tsx
@@ -1,4 +1,5 @@
 import type { ITweet } from "@/shared/types/tweet.interface";
+import Image from "next/image";
 
 interface ITweetProps {
   tweet: ITweet;
@@ -6,9 +7,12 @@ interface ITweetProps {
 
 export function Tweet({ tweet }: ITweetProps) {
   return (
-    <div>
-      <p>{tweet.text}</p>
-      <span>@{tweet.author}</span>
+    <div className="border border-white/10 rounded-xl p-4 bg-black text-white shadow-md">
+      <div className="flex items-center gap-3 mb-2">
+        <Image src={"/x-logo.svg"} alt="X logo" width={24} height={24} />
+        <span className="font-semibold">@{tweet.author}</span>
+      </div>
+      <p className="text-white/90">{tweet.text}</p>
     </div>
   );
 }
