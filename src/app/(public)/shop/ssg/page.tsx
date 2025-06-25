4b697c803983e9ac96cc87fab328c80a8d92b725
Helen <lenagreidon@mail.ru>
2025-06-25 14:55:53 +0300

    added content for ssg

--- src/app/(public)/shop/ssg/page.tsx
+++ src/app/(public)/shop/ssg/page.tsx
@@ -1,3 +1,15 @@
-export default function ShopSSGPage() {
-  return <div>SSG</div>;
+import { ProductCard, type IProduct } from "@/components/ProductCard";
+import { API_URL } from "@/constants";
+
+export default async function ShopSSGPage() {
+  const response = await fetch(API_URL);
+  const products = (await response.json()) as IProduct[];
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {products.map((el) => (
+        <ProductCard key={el.id} {...el} />
+      ))}
+    </div>
+  );
 }
