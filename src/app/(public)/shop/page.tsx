78132848311045aee2da90b4aaf1c1b7a1bb73dc
Helen <lenagreidon@mail.ru>
2025-06-25 14:59:07 +0300

    added content for ssr

--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,3 +1,15 @@
-export default function ShopPage() {
-  return <div>ShopPage</div>;
+import { ProductCard, type IProduct } from "@/components/ProductCard";
+import { API_URL } from "@/constants";
+
+export default async function ShopPage() {
+  const response = await fetch(API_URL, { cache: "no-store" });
+  const products = (await response.json()) as IProduct[];
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {products.map((el) => (
+        <ProductCard key={el.id} {...el} />
+      ))}
+    </div>
+  );
 }
