fc4fc2e649ca4765fc458a4e4e996c405c151b50
Helen <lenagreidon@mail.ru>
2025-06-25 15:15:12 +0300

    added generation dynamic metadata

--- src/app/(public)/u/[userid]/test/page.tsx
+++ src/app/(public)/u/[userid]/test/page.tsx
@@ -1,4 +1,14 @@
+import type { Metadata } from "next";
+
 type Params = { userid: string };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<Params>;
+}): Promise<Metadata> {
+  return { title: `User ${(await params).userid}` };
+}
 
 export default async function Test({ params }: { params: Promise<Params> }) {
   const { userid } = await params;
